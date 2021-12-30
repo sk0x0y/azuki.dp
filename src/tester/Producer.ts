7d0e153ba23b5ee060661ac9badd04decb6a7876
Yang Seok Yeong <seokyeong.yang@kroute.kr>
2021-12-31 08:32:02 +0900

    update: 송수신 테스트 코드

--- src/tester/Producer.ts
+++ src/tester/Producer.ts
@@ -7,13 +7,19 @@ export const run = async () => {
 
   await setInterval(() => {
     const date = new Date().toLocaleTimeString();
+    const randomNumber = Math.random();
+
     producer
       .send({
-        topic: "test-topic",
-        messages: [{ value: `Hello KafkaJS user! ${Math.random()} ${date}` }],
+        topic: "azuki.dp.action.test",
+        messages: [
+          {
+            value: `[홍어툴 - 홍어가 감지되었습니다!] Hello 전라도 Ray! 받아라 홍어 D-DOS 어택! ${randomNumber} ${date} Made by. 흑산도홍어 양.석.준\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지\nWarning 홍어 감지`,
+          },
+        ],
       })
       .then((res) => {
-        console.log(`[Producer] ${date}`);
+        console.log(`[Producer] ${date} ${randomNumber}`);
       });
   }, 50);
 };
