4a4182e0a58d58a9b1cb44b717173dd5c7ec0b56
Yang Seok Yeong <seokyeong.yang@kroute.kr>
2022-01-02 14:27:00 +0900

    update: 기존 메소드 지우고 헬퍼함수 추가

--- src/core/interface/repository/charactor.repository.ts
+++ src/core/interface/repository/charactor.repository.ts
@@ -3,11 +3,8 @@ import { Character } from "../../application/entities/Character";
 import { CharactorDTO } from "../dto/charactor.dto";
 
 @EntityRepository(Character)
-export class CharactorRepository extends Repository<Character> {
-  register(data: CharactorDTO) {
-    return this.createQueryBuilder()
-      .insert()
-      .into(Character)
-      .values({ ...data });
-  }
-}
+export class CharactorRepository extends Repository<Character> {}
+
+export const useCharactor = () => {
+  return new CharactorRepository();
+};
