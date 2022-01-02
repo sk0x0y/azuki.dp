3e08f19bdf39e2b07005b6b8cc843b3499bc9a24
Yang Seok Yeong <seokyeong.yang@kroute.kr>
2022-01-02 14:23:58 +0900

    update: 클라이언트 니즈에 맞게 스키마 업데이트

--- src/core/application/entities/Character.ts
+++ src/core/application/entities/Character.ts
@@ -38,5 +38,68 @@ export class Character {
   nationality: NationalityType;
 
   @Column({})
-  isActive: boolean;
+  nxclub_id: string;
+
+  @Column({ default: "연" })
+  server: string;
+
+  @Column({ nullable: true })
+  current_map: string;
+
+  @Column({ default: 0 })
+  job: number;
+
+  @Column({ default: 0 })
+  job_rank: number;
+
+  @Column({ nullable: true, default: "문파 이름 공간" })
+  clan: string;
+
+  @Column({ nullable: true, default: "문파 칭호 공간" })
+  clan_title: string;
+
+  @Column({ nullable: true, default: "칭호 공간" })
+  title?: string;
+
+  @Column({ default: 0 })
+  personality?: number;
+
+  @Column({ nullable: true, default: false })
+  active_pk?: boolean;
+
+  @Column({ nullable: true, default: true })
+  active_trade?: boolean;
+
+  @Column({ nullable: true, default: true })
+  active_group?: boolean;
+
+  @Column({ nullable: true, default: true })
+  alive: boolean;
+
+  @Column({ nullable: true, default: 50 })
+  current_hp: number;
+
+  @Column({ nullable: true, default: 50 })
+  max_hp: number;
+
+  @Column({ nullable: true, default: 50 })
+  current_mp: number;
+
+  @Column({ nullable: true, default: 50 })
+  max_mp: number;
+
+  @Column({ nullable: true, default: 0 })
+  money: number;
+
+  @Column({ nullable: true, default: 0 })
+  exp: number;
+
+  @Column({ nullable: true, default: 0 })
+  long: number;
+
+  @Column({ nullable: true, default: 0 })
+  lat: number;
+
+  @Column({ nullable: true, default: false })
+  blocked: boolean;
 }
--- src/core/interface/dto/charactor.dto.ts
+++ src/core/interface/dto/charactor.dto.ts
@@ -1,16 +1,16 @@
 export enum GenderType {
-  MALE,
-  FEMALE,
+  MALE, // 남자
+  FEMALE, // 여자
 }
 export enum GuardianType {
-  RED_BIRD,
-  BLACK_TORTOISE,
-  BLUE_DRAGON,
-  WHITE_TIGER,
+  RED_BIRD, // 주작
+  BLACK_TORTOISE, // 현무
+  BLUE_DRAGON, // 청룡
+  WHITE_TIGER, // 백호
 }
 export enum NationalityType {
-  BUYEO,
-  GOGURYEO,
+  BUYEO, // 부여
+  GOGURYEO, // 고구려
 }
 
 export interface CharactorDTO {
@@ -22,5 +22,26 @@ export interface CharactorDTO {
   gender: GenderType;
   guardian: GuardianType;
   nationality: NationalityType;
-  isActive: boolean;
+  nxclub_id: string;
+  server?: string;
+  current_map?: string;
+  job?: number;
+  job_rank?: number;
+  clan: string;
+  clan_title: string;
+  title?: string;
+  personality?: number;
+  active_pk?: boolean;
+  active_trade?: boolean;
+  active_group?: boolean;
+  alive?: boolean;
+  current_hp?: number;
+  max_hp?: number;
+  current_mp?: number;
+  max_mp?: number;
+  money?: number;
+  exp?: number;
+  long?: number;
+  lat?: number;
+  blocked?: boolean;
 }
