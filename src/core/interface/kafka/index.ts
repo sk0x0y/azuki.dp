7cc424c189f94c8567e0977f970f834308249bb3
Yang Seok Yeong <seokyeong.yang@kroute.kr>
2022-01-02 14:25:44 +0900

    update: Kafka 브로커 OF 쪽으로 스위치

--- src/core/interface/kafka/index.ts
+++ src/core/interface/kafka/index.ts
@@ -3,7 +3,7 @@ import { WinstonLogCreator } from "../Logger";
 
 export const kafka = new Kafka({
   clientId: "azuki-dp",
-  brokers: ["192.168.0.100:9092"],
+  brokers: ["192.168.0.74:9092"],
   connectionTimeout: 3000,
   logLevel: logLevel.INFO,
   logCreator: WinstonLogCreator,
